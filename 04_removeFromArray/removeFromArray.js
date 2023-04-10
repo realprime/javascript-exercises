7044ba1057949623b8c8c7ec7e4e490ad126456b
realprime <hemanthsaikiran12@gmail.com>
2023-04-10 21:20:58 +0530

    Add solutions until removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+    //removes items belongs to args from arr.
+    return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
